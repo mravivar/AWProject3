1bea16bed8d8a11e1dcae4133d1e604b69600bff
Murali Ravivarma <mravivar@asu.edu>
2017-11-20 21:59:03 -0700

    Update server.py

--- server.py
+++ server.py
@@ -74,8 +74,6 @@ def register():
     error = None
     if request.method == 'POST':
         tuple=user_table.find_one({"user_id":request.form["username"]})
-        print("############################")
-        print(tuple)
         if tuple is None:
             user_table.insert({"user_id":request.form["username"], "password":request.form["password"].encode('utf8'), "tag":request.form["interests"]})
             return redirect(url_for('login'))
